8f53c12f10b71234c3514d20991d6e7d96c8124c
SijiaDavis <arwen@ksu.edu>
2016-12-30 18:00:10 -0500

    Add user auth before creating new post

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -9,22 +9,56 @@ RSpec.describe GramsController, type: :controller do
   end
   
   describe "grams#new action" do
-    it "should sucessfully show the new form" do
+    it "shoud require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
+    
+    it "should sucessfully show the new form if the user is signed in" do
+      user = User.create(
+        email: 'fakeuser@gmail.com',
+        password: 'welcome',
+        password_confirmation: 'welcome'
+      )
+      sign_in user
+      
       get :new
       expect(response).to have_http_status(:success)
     end
+    
   end
   
   describe "grams#create action" do
-    it "should successfully create a new post in the db" do
+    it "should require users to be logged in" do
+      post :create, gram: {message: 'Sup'}
+      expect(response).to redirect_to new_user_session_path
+    end
+    
+    
+    it "should successfully create a new post in the db if the user is signed in" do
+      user = User.create(
+        email: 'fakeuser@gmail.com',
+        password: 'welcome',
+        password_confirmation: 'welcome'
+      )
+      sign_in user
+      
       post :create, gram: {message: 'sup!'}
       expect(response).to redirect_to root_path
       
       gram = Gram.last
       expect(gram.message).to eq('sup!')
+      expect(gram.user).to eq(user)
     end
     
     it "should properly handle the case when the post has an empty message" do
+      user = User.create(
+        email: 'fakeuser@gmail.com',
+        password: 'welcome',
+        password_confirmation: 'welcome'
+      )
+      sign_in user
+      
       post :create, gram: {message: ''}
       expect(response).to have_http_status(:unprocessable_entity)
       expect(Gram.count).to eq 0
