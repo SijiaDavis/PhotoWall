e827d6630c56755a3326e3dbc41857c4a824461b
SijiaDavis <arwen@ksu.edu>
2016-12-30 14:07:03 -0500

    add validation to post

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -24,6 +24,12 @@ RSpec.describe GramsController, type: :controller do
       expect(gram.message).to eq('sup!')
     end
     
+    it "should properly handle the case when the post has an empty message" do
+      post :create, gram: {message: ''}
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(Gram.count).to eq 0
+    end
+    
     
   end
 end
